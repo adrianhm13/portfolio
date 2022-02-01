5cff38a39dbb7c24444ebddcbe1d1576a129576f
adrianhm13 <adrian.hervasmartinez@gmail.com>
2022-02-01 19:23:21 +0100

    Layout for avatar in About page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -6,16 +6,21 @@ export default function About() {
   return (
     <Section id="About">
       <PageTitle title="About" />
-      <Styled.Text>
-        Hello! my name is Adrian. I am a <span>self-taught web developer</span>.
-        My interest in programming came up when I was modeling 3D characters as
-        a hobby. At some point, I wanted to build a visual novel with the models
-        I created with Unity, the game engine. I had to learn how to interpret
-        the scripts of other developers and modify them to adapt them to the
-        visual novel. In this process is when I realized that I enjoyed
-        programming. The feeling of seeing something working through code was
-        extremely rewarding, so I decided to become a web developer.
-      </Styled.Text>
+      <Styled.Content>
+        <Styled.Avatar>
+          A
+        </Styled.Avatar>
+        <Styled.Text>
+          Hello! my name is Adrian. I am a <span>self-taught web developer</span>.
+          My interest in programming came up when I was modeling 3D characters as
+          a hobby. At some point, I wanted to build a visual novel with the models
+          I created with Unity, the game engine. I had to learn how to interpret
+          the scripts of other developers and modify them to adapt them to the
+          visual novel. In this process is when I realized that I enjoyed
+          programming. The feeling of seeing something working through code was
+          extremely rewarding, so I decided to become a web developer.
+        </Styled.Text>
+      </Styled.Content>
     </Section>
   );
 }
--- src/pages/About/styled.ts
+++ src/pages/About/styled.ts
@@ -1,6 +1,18 @@
 import styled from "@emotion/styled";
 
+export const Content = styled.div({
+  display: 'flex',
+  flexDirection: 'row',
+  "@media (max-width: 600px)": {
+    flexWrap: 'wrap'
+  }
+})
+
+export const Avatar = styled.div({
+  width: '30%',
+})
 export const Text = styled.p((props) => ({
   ...props.theme.typography.paragraph,
   lineHeight: "2rem",
+  width: '70%',
 }));
